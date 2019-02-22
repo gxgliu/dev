94fdee19a77ed93bc657094fcc1271784ab6d021
liu <liuganxiang@gmail.com>
2019-02-23 02:58:22 +0900

    modify hello.go

--- go/hello.go
+++ go/hello.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Printf("Hello World!\n")
+
+	tz, _ := time.LoadLocation("Asia/Tokyo")
+	tm := time.Date(2019, 2, 22, 10, 30, 50, 0, tz)
+	fmt.Println(tm)
+	fmt.Println(tm.Unix())
+	fmt.Println(time.Now().Unix())
+	fmt.Println(time.Unix(1550799050, 0))
 }
