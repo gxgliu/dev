6caead8892516a7e85e17f179fcf0ca788e385f4
liu <liuganxiang@gmail.com>
2019-03-13 23:30:30 +0900

    add php/unittest

---
+++ php/unittest/tests/src/Math.php
@@ -0,0 +1,13 @@
+<?php
+ 
+namespace Math;
+ 
+class Math {
+    public function Add($a, $b) {
+        return $a + $b;
+    }
+ 
+    public function Sub($a, $b) {
+        return $a + $b;
+    }
+}
